4fa80b160fabc1eb5be09844da19d159b1676ee1
ffazzini <ffrancesco83@gmail.com>
2018-09-11 08:42:03 +0100

    remove unecessary componentscan

--- activiti-spring-connector/src/main/java/org/activiti/spring/connector/autoconfigure/ConnectorAutoConfiguration.java
+++ activiti-spring-connector/src/main/java/org/activiti/spring/connector/autoconfigure/ConnectorAutoConfiguration.java
@@ -25,7 +25,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "org.activiti.spring.connector")
 public class ConnectorAutoConfiguration {
 
 
