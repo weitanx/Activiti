5b0fe3d08faa9d8f5efab2b0ae9ed03256a28cca
ffazzini <ffrancesco83@gmail.com>
2018-09-11 08:42:30 +0100

    remove unecessary import

--- activiti-spring-connector/src/main/java/org/activiti/spring/connector/autoconfigure/ConnectorAutoConfiguration.java
+++ activiti-spring-connector/src/main/java/org/activiti/spring/connector/autoconfigure/ConnectorAutoConfiguration.java
@@ -21,7 +21,6 @@ import org.activiti.spring.connector.ConnectorService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
